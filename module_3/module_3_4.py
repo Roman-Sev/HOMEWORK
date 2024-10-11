d8ce5d78ed3d9c1a59c96b18610022f80365b043
Roman-Sev <romankomarov89@mail.ru>
2024-10-11 23:20:47 +0300

    ДОМАШНЯЯ РАБОТА

--- module_3/module_3_4.py
+++ module_3/module_3_4.py
@@ -4,7 +4,8 @@ def single_root_words(root_word, *other_words):
     for word in other_words:
         word_lower = word.lower()
         if root_word_lower in word_lower or word_lower in root_word_lower:
-            return same_words
+            same_words.append(word)
+    return same_words
 
 
 result = single_root_words("able", "Disablement", "Table", "capable", "label", "Able")
