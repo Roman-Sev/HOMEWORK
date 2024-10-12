2c44b1d6c9bd597589894f456b4d2f26fc3aeacd
Roman-Sev <romankomarov89@mail.ru>
2024-10-12 19:05:57 +0300

    ДОМАШНЯЯ РАБОТА

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -2,8 +2,12 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+        remaining_product = get_multiplied_digits(int(str_number[1:]))
+        if remaining_product == 0:
+            return first
+        else:
+            return first * remaining_product
     else:
         return first
-result = get_multiplied_digits(12345)
+result = get_multiplied_digits(123405)
 print(result)
