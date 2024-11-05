21f6e709ddf03b9278e914f1e817d676e3a50310
Roman-Sev <romankomarov89@mail.ru>
2024-11-05 13:08:01 +0300

    ДОМАШНЯЯ РАБОТА

---
+++ module_5/module_5_3.py
@@ -0,0 +1,56 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+            else:
+                print("Такого этажа не существует")
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f"Название:{self.name}, кол-во этажей:{self.number_of_floors}"
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return NotImplemented
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return NotImplemented
+    def __ne__(self, other):
+            if isinstance(other, House):
+                return self.number_of_floors != other.number_of_floors
+            return True
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return NotImplemented
+    def __radd__(self, value):
+        return self.__add__(value)
+    def __iadd__(self,value):
+        return self.__add__(value)
+
+
+house1 = House("ЖК ЭЛЬБРУС", 30)
+house2 = House("ЖК Каскад", 25)
+print(house1 == house2)
+print(house1 > house2)
+print(house1 < house2)
+house1 + 5
+print(house1)
+
